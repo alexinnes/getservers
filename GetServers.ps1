e488a425985c150d878eae47b737c8120a49dc0e
Alex Innes <alexnixoninnes@gmail.com>
2019-01-15 08:29:07 +0000

    Added files

---
+++ GetServers.ps1
@@ -0,0 +1,20 @@
+﻿#Requires –Modules ActiveDirectory
+<#
+.Synopsis
+   Gets all servers
+.DESCRIPTION
+   Gets ADComputer object and returns anything that has "server" in its operating system property. Needs to be run on a domain controller 2008 and above.
+.EXAMPLE
+   get-Servers
+#>
+function Get-Servers
+{
+    $servers = Get-adcomputer -Filter {OperatingSystem -like "*Server*"}
+    $liveServer = @()
+    Foreach($serv in $servers){
+        if(Test-Connection $serv.name -Count 2 -Quiet){
+            $liveServer += $serv.name
+        }
+    }
+    return $liveServer
+}
---
+++ ReadMe.md
@@ -0,0 +1,6 @@
+# Get Servers
+Simple script to get all server in a domain (if they are listed in Active Directory)
+
+```
+Get-Servers
+```
